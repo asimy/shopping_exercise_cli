e352521ad185a3a57dca834feef46369f06c6e15
Glen Aultman-Bettridge <gaultmanbettridge@fluidtruck.com>
2020-06-22 12:03:58 -0600

    renames & cleans up implementation of list parser
    As I was writing the tests I realized that my initial name was entirely unclear. Changed it to something hopefully clearer and added some state. Also updated spec to cover invalid lists better

--- price_calculator.rb
+++ price_calculator.rb
@@ -1,7 +1,23 @@
+# frozen_string_literal: true
+
 # to keep the file structure as simple as possible, I'm creating all my classes in this file
 
-class PurchaseReader
-    def valid?(shopping_list)
-        /^([a-z]+, *)*[a-z]+$/.match?(shopping_list)
-    end
+class ShoppingListParser
+  attr_reader :shopping_input, :tallied_list
+
+  def initialize(shopping_input)
+    @shopping_input = shopping_input
+    @tallied_list = {}
+    @tallied_list = convert_to_list if valid?
+  end
+
+  def valid?
+    /^([a-z]+, *)*[a-z]+$/.match?(shopping_input)
+  end
+
+  def convert_to_list
+    cleaned_input = shopping_input.gsub(/\s+/, '')
+    shopping_array = cleaned_input.split(',')
+    shopping_list = shopping_array.tally
+  end
 end
--- spec/purchase_reader_spec.rb
+++ spec/purchase_reader_spec.rb
@@ -2,24 +2,27 @@
 
 require_relative '../price_calculator'
 
-describe PurchaseReader do
+describe ShoppingListParser do
   let(:valid_shopping_list) { 'milk,milk, bread,banana,bread,bread,bread,milk,apple' }
-  let(:invalid_shopping_list) {'milk;milk; bread;banana;bread;bread;bread;milk;apple'}
-  let(:pr) {PurchaseReader.new}
+  let(:invalid_shopping_list) { 'milk;milk; bread;banana;bread;bread;bread;milk;apple' }
 
   it 'validates a valid shopping list' do
-    expect(pr.valid?(valid_shopping_list)).to be true
+    parser = ShoppingListParser.new(valid_shopping_list)
+    expect(parser.valid?).to be true
   end
 
   it 'does not validate an invalid shopping list' do
-    expect(pr.valid?(invalid_shopping_list)).to be false
-  end
-  
-  it 'converts valid input into an array' do
-    expect(pr.convert_to_list(valid_shopping_list)).to equal %w[milk milk bread banana bread bread bread milk apple]
+    parser = ShoppingListParser.new(invalid_shopping_list)
+    expect(parser.valid?).to be false
   end
 
-  # it 'reports an error for invalid input' do
-    
-  # end
+  it 'converts valid input into a tallied list' do
+    parser = ShoppingListParser.new(valid_shopping_list)
+    expect(parser.tallied_list).to eq({ 'milk' => 3, 'bread' => 4, 'banana' => 1, 'apple' => 1 })
+  end
+
+  it 'converts invalid input into an empty list' do
+    parser = ShoppingListParser.new(invalid_shopping_list)
+    expect(parser.tallied_list).to eq({})
+  end
 end
