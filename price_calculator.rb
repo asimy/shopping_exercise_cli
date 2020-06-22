f8c615ce39e11a3096836f641191f152ff03c840
Glen Aultman-Bettridge <gaultmanbettridge@fluidtruck.com>
2020-06-22 11:22:12 -0600

    Adds validation

--- price_calculator.rb
+++ price_calculator.rb
@@ -1,1 +1,7 @@
 # to keep the file structure as simple as possible, I'm creating all my classes in this file
+
+class PurchaseReader
+    def valid?(shopping_list)
+        /^([a-z]+, *)*[a-z]+$/.match?(shopping_list)
+    end
+end
--- spec/purchase_reader_spec.rb
+++ spec/purchase_reader_spec.rb
@@ -5,10 +5,17 @@ require_relative '../price_calculator'
 describe PurchaseReader do
   let(:valid_shopping_list) { 'milk,milk, bread,banana,bread,bread,bread,milk,apple' }
   let(:invalid_shopping_list) {'milk;milk; bread;banana;bread;bread;bread;milk;apple'}
+  let(:pr) {PurchaseReader.new}
 
+  it 'validates a valid shopping list' do
+    expect(pr.valid?(valid_shopping_list)).to be true
+  end
+
+  it 'does not validate an invalid shopping list' do
+    expect(pr.valid?(invalid_shopping_list)).to be false
+  end
+  
   it 'converts valid input into an array' do
-    pr = PurchaseReader.new
-    expect(pr.valid?(valid_shopping_list)).to be_true
     expect(pr.convert_to_list(valid_shopping_list)).to equal %w[milk milk bread banana bread bread bread milk apple]
   end
 
