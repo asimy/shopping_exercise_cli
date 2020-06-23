e15e1b299fa049cfd895385ecab391d7c928b191
Glen Aultman-Bettridge <gaultmanbettridge@fluidtruck.com>
2020-06-22 19:32:14 -0600

    Add a bunch of specs around formatting the bill

--- spec/purchase_reader_spec.rb
+++ spec/purchase_reader_spec.rb
@@ -56,13 +56,67 @@ describe PricingCalculator do
     bill.sort
   end
 
+  let(:purchase_bill_with_sales) do
+    bill = []
+    bill << LineItem.new('milk', 3, 8.97, 2.94)
+    bill << LineItem.new('bread', 4, 8.17, 0.51)
+    bill << LineItem.new('apple', 1, 0.89, 0)
+    bill << LineItem.new('banana', 1, 0.99, 0)
+    bill.sort
+  end
+
   it 'returns correct pricing with no items' do
     calc = PricingCalculator.new([])
-    expect(calc.shopping_bill(no_item_list)).to eq([])
+    expect(calc.shopping_items(no_item_list)).to eq([])
   end
 
   it 'returns correct pricing with items and no sales' do
     calc = PricingCalculator.new(no_sale_price_list)
-    expect(calc.shopping_bill(tallied_list).sort).to eq(purchase_bill_no_sales)
+    expect(calc.shopping_items(tallied_list).sort).to eq(purchase_bill_no_sales)
+  end
+
+  it 'returns correct pricing with items and sales' do
+    calc = PricingCalculator.new(sale_price_list)
+    expect(calc.shopping_items(tallied_list).sort).to eq(purchase_bill_with_sales)
+  end
+end
+
+describe BillFormatter do
+  let(:purchase_bill_with_sales) do
+    bill = []
+    bill << LineItem.new('milk', 3, 8.97, 2.94)
+    bill << LineItem.new('bread', 4, 8.17, 0.51)
+    bill << LineItem.new('apple', 1, 0.89, 0)
+    bill << LineItem.new('banana', 1, 0.99, 0)
+    bill.sort
+  end
+
+  let(:formatted_bill) do
+    bill = <<~EOF
+      Item     Quantity      Price
+      --------------------------------------
+      Apple     1            $0.89
+      Banana    1            $0.99
+      Bread     4            $8.17
+      Milk      3            $8.97
+
+      Total price: $19.02
+      You saved $3.45 today.
+    EOF
+  end
+
+  it 'calculates the total price' do
+    formatter = BillFormatter.new(purchase_bill_with_sales)
+    expect(formatter.total_price).to eq 19.02
+  end
+
+  it 'calculates the total savings' do
+    formatter = BillFormatter.new(purchase_bill_with_sales)
+    expect(formatter.total_savings).to eq 3.45
+  end
+
+  it 'returns a formatted text table from a shopping list' do
+    formatter = BillFormatter.new(purchase_bill_with_sales)
+    expect(formatter.printable_bill).to eq formatted_bill
   end
 end
